ffcedc67d3a1ebdeeb3bd14485e42e3304b51021
Mattia Natali <matitalatina@gmail.com>
2024-04-12 16:50:13 +0200

    From beer to coffee

--- src/components/sponsor/BuyBeerBtn.tsx
+++ src/components/sponsor/BuyBeerBtn.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { ButtonLink } from '../ui/Button';
 
 export const BuyBeerButton: React.FC<{className?: string}> = ({ className }) => {
-  return (<ButtonLink className={className} href="https://www.buymeacoffee.com/mattianatali" target="_blank">🍺 Offrimi una birra</ButtonLink>);
+  return (<ButtonLink className={className} href="https://www.buymeacoffee.com/mattianatali" target="_blank">☕️ Offrimi un caffè</ButtonLink>);
 };
